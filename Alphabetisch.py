c9f97a80070e54cfef638e5155e5c318694b4e2d
Fio Ambrosetti <fio.ambrosetti@edeka.de>
2026-06-08 13:40:49 +0200

    letzte Aufgabe auch fertig

--- Alphabetisch.py
+++ Alphabetisch.py
@@ -1,1 +1,12 @@
-string = input("Bitte einen beliebigen Satz eingeben!")
+string = input("Bitte einen beliebigen Satz eingeben:  ")
+wordlist = []
+word = ""
+for char in string:
+    if not char == " ":
+        word += char
+    else:
+        wordlist.append(word.lower())
+        word = ""
+wordlist.append(word.lower())
+
+print(sorted(wordlist))
